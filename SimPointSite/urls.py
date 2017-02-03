99623b870cbb8e339f1306527f76859824354f7b
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-02-04 02:12:43 +0800

    finish djongo tutorial

--- SimPointSite/urls.py
+++ SimPointSite/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^webapp/', include('webapp.urls')),
     url(r'^personal/', include('personal.urls')),
-    url(r'^$', include('personal.urls')),
+    # url(r'^', include('personal.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- personal/views.py
+++ personal/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html' , {'content': ['if you would','fsdaf']})
+
+
+
